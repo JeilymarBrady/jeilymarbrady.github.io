dc219f8728c63a9c24f6ccb9e1e0454c3baa2cef
JEILYMAR BRADY <jeilymarb@gmail.com>
2015-08-07 20:44:28 -0500

    added a lot

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-/*
-  Bounce down animation needs value ‘infinite’ on animation-iteration-count property of animation.
-
-  animation-iteration-count: infinite
-
-  Can have this in a shorthand too, like in the Codepen:
-
-  animation: bounce-down 2s ease infinite;
-
-  And [sometimes it makes sense to postpone the execution of css-animations and transitions until the webpage has fully loaded](http://codepen.io/atelierbram/pen/GFyiC) …
-
-*/
-/*
- * http://blog.simonwillison.net/post/57956760515/addloadevent
- * http://www.sitepoint.com/closures-and-executing-javascript-on-page-load/
-*/
+//////////////////////////////
 function addLoadEvent(func) {
   var oldonload = window.onload;
   if (typeof window.onload != 'function') {
@@ -44,6 +29,10 @@ $(window).scroll(function() {
       $('#downArrow').fadeIn();
      }
  });
+//////////////////////////////
+
+
+//////////////////////////////
 /* ***************************
   Code from CSS-Tricks Forum
   author:  Chris McClean
@@ -51,81 +40,57 @@ $(window).scroll(function() {
 ***************************** */
 
 $(document).ready(function() {
-  // $('#fullpage').fullpage({
-  //       anchors:['top', 'about', 'resume', 'portfolio', 'contactMe']
-  //   });
-  // $('#fullpage').show('slow/400/fast', function() {
-  //       //Navigation
-  //       menu: false,
-  //       lockAnchors: false,
-  //       anchors:['firstPage', 'secondPage'],
-  //       navigation: false,
-  //       navigationPosition: 'right',
-  //       navigationTooltips: ['firstSlide', 'secondSlide'],
-  //       showActiveTooltip: false,
-  //       slidesNavigation: true,
-  //       slidesNavPosition: 'bottom',
-
-  //       //Scrolling
-  //       css3: true,
-  //       scrollingSpeed: 700,
-  //       autoScrolling: true,
-  //       fitToSection: true,
-  //       scrollBar: false,
-  //       easing: 'easeInOutCubic',
-  //       easingcss3: 'ease',
-  //       loopBottom: false,
-  //       loopTop: false,
-  //       loopHorizontal: true,
-  //       continuousVertical: false,
-  //       normalScrollElements: '#element1, .element2',
-  //       scrollOverflow: false,
-  //       touchSensitivity: 15,
-  //       normalScrollElementTouchThreshold: 5,
-
-  //       //Accessibility
-  //       keyboardScrolling: true,
-  //       animateAnchor: true,
-  //       recordHistory: true,
-
-  //       //Design
-  //       controlArrows: true,
-  //       verticalCentered: true,
-  //       resize : false,
-  //       sectionsColor : ['#ccc', '#fff'],
-  //       paddingTop: '3em',
-  //       paddingBottom: '10px',
-  //       fixedElements: '#header, .footer',
-  //       responsiveWidth: 0,
-  //       responsiveHeight: 0,
-
-  //       //Custom selectors
-  //       sectionSelector: '.section',
-  //       slideSelector: '.slide',
-
-  //       //events
-  //       onLeave: function(index, nextIndex, direction){},
-  //       afterLoad: function(anchorLink, index){},
-  //       afterRender: function(){},
-  //       afterResize: function(){},
-  //       afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-  //       onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-  //   });
    $('a[href*=#]').bind('click', function(e) {
   e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
 
   var target = $(this).attr("href"); //Get the target
 
   // perform animated scrolling by getting top-position of target-element and set it as scroll target
-  $('html, body').stop().animate({ scrollTop: $(target).offset().top - 80}, 1500, function() {
+  $('html, body').stop().animate({ scrollTop: $(target).offset().top - 69.5}, 1500, function() {
        //location.hash = target; //attach the hash (#jumptarget) to the pageurl
   });
 
   return false;
    });
-
-  //Toggle image for name
-  // $("#jeily").click(function(){
-  //   $("#jeily img").toggle();
-  // });
 });
+//////////////////////////////
+
+
+//////////////////////////////
+// Portfolio Slides
+var currentIndex = 0,
+  items = $('.portfolioContainer div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.portfolioContainer div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
+//////////////////////////////
